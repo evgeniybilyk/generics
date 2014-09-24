9dbedbfbbc9a951af8f78d97fe0eeb3c1d318c4f
evgeniybilyk <evgeniy.bilyk@gmail.com>
2014-09-24 16:46:42 +0300

    Create new objects in method main

--- src/jMain.java
+++ src/jMain.java
@@ -6,11 +6,16 @@ public class jMain {
         Teacher<Integer, String> tch1 = new Teacher<Integer, String>(48756, "Василиса Петровна");
         Teacher<Integer, String> tch2 = new Teacher<Integer, String>(77897, "Василиса Петровна");
 
+        Box<Integer, String> box1 = new Box<Integer, String>(265894,"Коробка 1");
+        Bag<Integer, String> bag1 = new Bag<Integer, String>(23654, "Сумка с вещами");
+
         Bus<iCargo> bus = new Bus<iCargo>();
         bus.carry(st1,"в школу");
         bus.carry(st2,"в школу");
         bus.carry(tch1,"в школу");
         bus.carry(tch2,"в школу");
+        bus.carry(box1,"в аэропорт");
+        bus.carry(bag1,"в аэропорт");
 
         bus.countTakenPlaces();
     }
